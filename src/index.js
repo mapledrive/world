53d0847f078578770990e9eb377bbceed924f043
mapledrive <sait.sharifullin@gmail.com>
2022-09-21 02:47:01 +0500

    more drop boxes

--- src/index.js
+++ src/index.js
@@ -8,8 +8,20 @@ import styled from 'styled-components';
 
 export const Container = () => {
   const [boxes, setBoxes] = useState({
-    a: { top: 50, left: 100, title: 'Drag me around' },
-    b: { top: 125, left: 100, title: 'Drag me around' },
+    a: { top: 50, left: 100, title: 'configure Store -saucezar' },
+    b: { top: 100, left: 100, title: 'store - sun or lamp' },
+    c: { top: 150, left: 100, title: 'provider-electricity or light' },
+    d: { top: 200, left: 100, title: 'App-mid field heap of functions' },
+    e: { top: 250, left: 100, title: 'createSlice-white small house' },
+    f: { top: 300, left: 100, title: 'initialState' },
+    g: { top: 350, left: 100, title: 'name' },
+    h: { top: 400, left: 100, title: 'reducers' },
+    j: { top: 450, left: 100, title: 'slice' },
+    k: { top: 500, left: 100, title: 'actions - object' },
+    l: { top: 550, left: 100, title: 'reducer - object' },
+    m: { top: 600, left: 100, title: 'useSelector-shlang' },
+    n: { top: 650, left: 100, title: 'useDispatch safe/pistol' },
+    o: { top: 700, left: 100, title: 'dispatch' },
   });
   const moveBox = useCallback(
     (id, left, top) => {
@@ -96,7 +108,7 @@ const StyledBox = styled.div`
 
 const StyledContainer = styled.div`
   width: 100%;
-  height: 500px;
+  height: 800px;
   border: 1px solid black;
   position: relative;
 `;
