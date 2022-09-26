ac1a37af9deea0a025bbbcf4120d2f25917faeeb
mapledrive <sait.sharifullin@gmail.com>
2022-09-26 15:18:49 +0500

    styles separated from components

--- src/LeftSidebar.js
+++ src/LeftSidebar.js
@@ -1,5 +1,13 @@
-import styled from 'styled-components';
 import CubeIcon from './CubeIcon';
+import {
+  OverlayTopMenuSidebar,
+  LeftMenu,
+  BaseButtonSidebar,
+  LoadingContentWrapperSidebar,
+  ContentWrapperSidebar,
+  BaseIconSidebar,
+  SettingsIconSidebar,
+} from './style';
 
 const sidebarData = [
   { id: 1, name: 'Error', mainColor: 'red' },
@@ -15,104 +23,27 @@ const sidebarData = [
 
 export const LeftSidebar = ({ showModal }) => {
   return (
-    <OverlayTopMenu>
+    <OverlayTopMenuSidebar>
       <LeftMenu>
         {sidebarData.map(data => (
-          <BaseButton
+          <BaseButtonSidebar
             key={data.id}
             id={data.id}
             onClick={() => showModal(data.name)}
           >
-            <LoadingContentWrapper>
-              <ContentWrapper mainColor={data.mainColor}>
-                <BaseIcon>
-                  <SettingsIcon>
+            <LoadingContentWrapperSidebar>
+              <ContentWrapperSidebar mainColor={data.mainColor}>
+                <BaseIconSidebar>
+                  <SettingsIconSidebar>
                     <CubeIcon mainColor={data.mainColor} />
-                  </SettingsIcon>
-                </BaseIcon>
+                  </SettingsIconSidebar>
+                </BaseIconSidebar>
                 {data.name}
-              </ContentWrapper>
-            </LoadingContentWrapper>
-          </BaseButton>
+              </ContentWrapperSidebar>
+            </LoadingContentWrapperSidebar>
+          </BaseButtonSidebar>
         ))}
       </LeftMenu>
-    </OverlayTopMenu>
+    </OverlayTopMenuSidebar>
   );
 };
-
-const OverlayTopMenu = styled.section`
-  transform: translate(11px, -50%);
-  top: 50vh;
-  left: 0px;
-  transition: transform 250ms;
-  position: fixed;
-`;
-
-const LeftMenu = styled.div`
-  border-radius: 25px;
-  background: #393939;
-  display: flex;
-  flex-direction: column;
-  padding: 25px 0;
-  position: relative;
-  transition: padding 400ms;
-  box-shadow: 1px 3px 6px 0 rgba(0, 0, 0, 0.2);
-`;
-
-const BaseButton = styled.button`
-  font-size: 14px;
-  color: #fff;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 9px 0;
-  max-height: 100px;
-  transition: max-height 150ms, padding 300ms;
-  position: relative;
-  background: #393939;
-  border: none;
-  cursor: pointer;
-  outline: 0 !important;
-`;
-
-const LoadingContentWrapper = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  width: 75px;
-`;
-
-const ContentWrapper = styled.div`
-  position: relative;
-  width: 75px;
-  transition: all 300ms;
-  margin: 0;
-  padding: 28px 0 0 0;
-  border: 0;
-  font-size: 14px;
-  color: ${props => props.mainColor};
-  font-family: 'Open Sans', sans-serif;
-  overflow: hidden;
-`;
-
-const BaseIcon = styled.div`
-  top: 0;
-  left: 50%;
-  transform: translate(-50%, 0);
-  position: absolute;
-  height: 28px;
-  width: 28px;
-  transition: all 300ms;
-  display: block;
-  margin: 0;
-  padding: 0;
-  border: 0;
-`;
-
-const SettingsIcon = styled.svg`
-  transition: fill 300ms;
-  fill: #fff;
-  display: block;
-  height: 100%;
-  width: 100%;
-`;
---
+++ src/style.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+export const OverlayTopMenuSidebar = styled.section`
+  transform: translate(11px, -50%);
+  top: 50vh;
+  left: 0px;
+  transition: transform 250ms;
+  position: fixed;
+`;
+
+export const LeftMenu = styled.div`
+  border-radius: 25px;
+  background: #393939;
+  display: flex;
+  flex-direction: column;
+  padding: 25px 0;
+  position: relative;
+  transition: padding 400ms;
+  box-shadow: 1px 3px 6px 0 rgba(0, 0, 0, 0.2);
+`;
+
+export const BaseButtonSidebar = styled.button`
+  font-size: 14px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 9px 0;
+  max-height: 100px;
+  transition: max-height 150ms, padding 300ms;
+  position: relative;
+  background: #393939;
+  border: none;
+  cursor: pointer;
+  outline: 0 !important;
+`;
+
+export const LoadingContentWrapperSidebar = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 75px;
+`;
+
+export const ContentWrapperSidebar = styled.div`
+  position: relative;
+  width: 75px;
+  transition: all 300ms;
+  margin: 0;
+  padding: 28px 0 0 0;
+  border: 0;
+  font-size: 14px;
+  color: ${props => props.mainColor};
+  font-family: 'Open Sans', sans-serif;
+  overflow: hidden;
+`;
+
+export const BaseIconSidebar = styled.div`
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, 0);
+  position: absolute;
+  height: 28px;
+  width: 28px;
+  transition: all 300ms;
+  display: block;
+  margin: 0;
+  padding: 0;
+  border: 0;
+`;
+
+export const SettingsIconSidebar = styled.svg`
+  transition: fill 300ms;
+  fill: #fff;
+  display: block;
+  height: 100%;
+  width: 100%;
+`;
