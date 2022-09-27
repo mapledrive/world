c5f2d01159ad75e16cc9a0eb442727d2b301051b
mapledrive <sait.sharifullin@gmail.com>
2022-09-28 01:10:11 +0500

    pics

---
+++ src/Modal.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import styled from 'styled-components';
+import RotatingCube from './RotatingCube';
+// https://codepen.io/alligatorio/pen/aYzMKL
+
+export const Modal = ({ builtin, hideModal }) => {
+  if (builtin === '') return null;
+
+  const propertiez = getAllPropertyNames(builtin);
+  console.log(propertiez.toString());
+  return (
+    <StyledModalMain>
+      {builtin}
+      <RotatingCube />
+      {propertiez.toString()}
+      <StyledClose onClick={hideModal}>
+        <SvgComponent />
+      </StyledClose>
+    </StyledModalMain>
+  );
+};
+
+const SvgComponent = props => (
+  <svg
+    className='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv'
+    aria-hidden='true'
+    viewBox='0 0 24 24'
+    data-testid='CloseIcon'
+    fill='rgb(158, 158, 158)'
+    {...props}
+  >
+    <path d='M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z' />
+  </svg>
+);
+
+const StyledClose = styled.div`
+  display: inline-flex;
+  -moz-box-align: center;
+  align-items: center;
+  -moz-box-pack: center;
+  justify-content: center;
+  box-sizing: border-box;
+  background-color: transparent;
+  border: 0px none;
+  margin: 0px;
+  cursor: pointer;
+  user-select: none;
+  appearance: none;
+  text-decoration: none;
+  text-align: center;
+  font-size: 15px;
+  padding: 8px;
+  border-radius: 50%;
+  overflow: visible;
+  transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  position: absolute;
+  right: 8px;
+  top: 8px;
+  color: rgb(158, 158, 158);
+  width: 40px;
+  height: 40px;
+  &:hover {
+    background: #f5f5f5;
+  }
+`;
+
+const StyledModalMain = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 500px;
+  width: 500px;
+  background: #fff;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: rgba(0, 0, 0, 0.87);
+  transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 11px 15px -7px,
+    rgba(0, 0, 0, 0.14) 0px 24px 38px 3px, rgba(0, 0, 0, 0.12) 0px 9px 46px 8px;
+`;
+
+function getAllPropertyNames(obj) {
+  let builtinObj;
+  if (obj === 'Number') {
+    builtinObj = (5.3).__proto__.constructor;
+  }
+  if (obj === 'String') {
+    builtinObj = ''.__proto__.constructor;
+  }
+  if (obj === 'Boolean') {
+    builtinObj = true.__proto__.constructor;
+  }
+  if (obj === 'Object') {
+    builtinObj = {}.__proto__.constructor;
+  }
+  if (obj === 'Array') {
+    builtinObj = [].__proto__.constructor;
+  }
+  if (obj === 'Function') {
+    builtinObj = x => x.__proto__.constructor;
+  }
+  if (obj === 'Date') {
+    builtinObj = new Date.__proto__.constructor();
+  }
+  if (obj === 'RegExp') {
+    builtinObj = /one/.__proto__.constructor;
+  }
+  if (obj === 'Error') {
+    builtinObj = Error.__proto__.constructor;
+  }
+  if (obj === 'Promise') {
+    builtinObj = new Promise.__proto__.constructor();
+  }
+  if (obj === 'Symbol') {
+    builtinObj = Symbol().__proto__.constructor;
+  }
+  if (obj === 'Generator') {
+    builtinObj = (function* () {})().__proto__.constructor;
+  }
+  if (obj === 'GeneratorF') {
+    builtinObj = function* () {}.constructor;
+  }
+  if (obj === 'Map') {
+    builtinObj = new Map().__proto__.constructor;
+  }
+  if (obj === 'Set') {
+    builtinObj = new Set().__proto__.constructor;
+  }
+  if (obj === 'WeakMap') {
+    builtinObj = new new WeakMap().__proto__.constructor();
+  }
+  if (obj === 'WeakSet') {
+    builtinObj = new WeakSet().__proto__.constructor;
+  }
+  if (obj === 'TypedArray') {
+    builtinObj = new Int8Array(8).__proto__.constructor;
+  }
+  if (obj === 'Reflect') {
+    builtinObj = null;
+  }
+  if (obj === 'Proxy') {
+    builtinObj = new Proxy({}, {});
+  }
+  if (obj === 'ArrayBuffer') {
+    builtinObj = new ArrayBuffer(8).__proto__.constructor;
+  }
+  if (obj === 'DataView') {
+    builtinObj = new DataView(new ArrayBuffer(16), 0).__proto__.constructor;
+  }
+  if (obj === 'AsyncFunc') {
+    builtinObj = async function () {}.constructor;
+  }
+
+  return [...new Set(Object.getOwnPropertyNames(builtinObj))];
+}
--- src/index.js
+++ src/index.js
@@ -8,9 +8,22 @@ import { DndProvider, useDrop, useDrag } from 'react-dnd';
 import { TopNavigation } from './TopNavigation';
 import { BottomNavigation } from './BottomNavigation';
 import { LeftSidebar } from './LeftSidebar';
-import { ModalComponent } from './ModalComponent';
+import { Modal } from './Modal';
 import OpenSans from './fonts/opensans.woff2';
 import Roboto from './fonts/roboto.woff2';
+import img from './caezar.jpg';
+import lamp from './lamp.svg';
+import frog from './frog.jpg';
+import provider from './provider.jpg';
+import app from './app.png';
+import createslice from './createslice.jpg';
+import name from './name.jpg';
+import initialstate from './initialstate.jpg';
+import useselector from './useselector.jpg';
+import safe from './safe.jpg';
+import disruptor from './disruptor.jpg';
+import bullet from './bullet.jpg';
+import slice from './slice.jpg';
 
 const App = () => {
   return (
@@ -41,20 +54,50 @@ const AppLayout = () => {
   };
 
   const [boxes, setBoxes] = useState({
-    a: { top: 50, left: 100, title: 'configure Store -saucezar' },
-    b: { top: 100, left: 100, title: 'store - sun or lamp' },
-    c: { top: 150, left: 100, title: 'provider-electricity or light' },
-    d: { top: 200, left: 100, title: 'App-mid field heap of functions' },
-    e: { top: 250, left: 100, title: 'createSlice-white small house' },
-    f: { top: 300, left: 100, title: 'initialState' },
-    g: { top: 350, left: 100, title: 'name' },
-    h: { top: 400, left: 100, title: 'reducers' },
-    j: { top: 450, left: 100, title: 'slice' },
-    k: { top: 500, left: 100, title: 'actions - object' },
-    l: { top: 550, left: 100, title: 'reducer - object' },
-    m: { top: 600, left: 100, title: 'useSelector-shlang' },
-    n: { top: 650, left: 100, title: 'useDispatch safe/pistol' },
-    o: { top: 700, left: 100, title: 'dispatch' },
+    a: {
+      top: 50,
+      left: 100,
+      title: 'configure Store',
+      image: `${img}`,
+    },
+    b: { top: 100, left: 100, title: 'store', image: `${lamp}` },
+    c: {
+      top: 150,
+      left: 100,
+      title: 'provider',
+      image: `${provider}`,
+    },
+    d: {
+      top: 200,
+      left: 100,
+      title: 'App-mid field heap of functions',
+      image: `${app}`,
+    },
+    e: {
+      top: 250,
+      left: 100,
+      title: 'createSlice-house',
+      image: `${createslice}`,
+    },
+    f: { top: 300, left: 100, title: 'initialState', image: `${initialstate}` },
+    g: { top: 350, left: 100, title: 'name', image: `${name}` },
+    h: { top: 400, left: 100, title: 'reducers', image: `${frog}` },
+    j: { top: 450, left: 100, title: 'slice', image: `${slice}` },
+    k: { top: 500, left: 100, title: 'actions - object', image: `${bullet}` },
+    l: { top: 550, left: 100, title: 'reducer - object', image: `${frog}` },
+    m: {
+      top: 600,
+      left: 100,
+      title: 'useSelector-shlang',
+      image: `${useselector}`,
+    },
+    n: {
+      top: 650,
+      left: 100,
+      title: 'useDispatch',
+      image: `${safe}`,
+    },
+    o: { top: 700, left: 100, title: 'dispatch', image: `${disruptor}` },
   });
 
   const moveBox = useCallback(
@@ -84,10 +127,11 @@ const AppLayout = () => {
   );
   return (
     <BasicLayout ref={drop}>
+      <Modal showModal={showModal} hideModal={hideModal} builtin={builtin} />
       {Object.keys(boxes).map(key => {
-        const { left, top, title } = boxes[key];
+        const { left, top, title, image } = boxes[key];
         return (
-          <Box key={key} id={key} left={left} top={top}>
+          <Box key={key} id={key} left={left} top={top} image={image}>
             {title}
           </Box>
         );
@@ -95,16 +139,11 @@ const AppLayout = () => {
       <LeftSidebar showModal={showModal} />
       <TopNavigation showModal={showModal} />
       <BottomNavigation showModal={showModal} />
-      <ModalComponent
-        showModal={showModal}
-        hideModal={hideModal}
-        builtin={builtin}
-      />
     </BasicLayout>
   );
 };
 
-const Box = ({ id, left, top, children }) => {
+const Box = ({ id, left, top, image, children }) => {
   const [{ isDragging }, drag] = useDrag(
     () => ({
       type: 'box',
@@ -119,7 +158,7 @@ const Box = ({ id, left, top, children }) => {
     return <div ref={drag} />;
   }
   return (
-    <StyledBox ref={drag} style={{ left, top }}>
+    <StyledBox ref={drag} image={image} style={{ left, top }}>
       {children}
     </StyledBox>
   );
@@ -164,12 +203,11 @@ export const GlobalStyle = createGlobalStyle`
 const BasicLayout = styled.div`
   background: grey;
   min-height: 100vh;
+  width: 100vw;
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
+  align-items: center;
+  justify-content: center;
   color: black;
-  position: relative;
 `;
 
 const StyledBox = styled.div`
@@ -177,8 +215,10 @@ const StyledBox = styled.div`
   position: absolute;
   border: 1px dashed gray;
   background-color: white;
-  height: 50px;
-  width: 200px;
+  background-image: url(${props => props.image});
+  background-repeat: no-repeat;
+  height: 150px;
+  width: 215px;
   padding: 10px;
   cursor: move;
   user-select: none;
